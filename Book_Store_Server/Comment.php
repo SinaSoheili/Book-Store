474c9b200f3a5a3ee0498641ffc97183fec057bd
SINA <sinasoheili79@gmail.com>
2020-03-03 23:55:55 +0330

    php file that get comment of specific book

---
+++ Book_Store_Server/Comment.php
@@ -0,0 +1,26 @@
+<?php
+
+    //Comment.php ? book_id =
+
+    require_once ('DB_Info.php');
+
+    if(! isset($_GET["book_id"]))
+    {
+        echo "request isn't valid";
+        exit();
+    }
+
+    $book_id = (int)$_GET["book_id"];
+
+    $db_comment = new mysqli(HOST , USER_NAME , PASSWORD , DB_NAME);
+    if($db_comment->connect_error === true)
+    {
+        echo "can not connect to databse";
+        exit();
+    }
+
+    $cmd = "SELECT * FROM ".TBL_COMMENT." WHERE BOOK_ID = ".$book_id;
+    $query_result = $db_comment->query($cmd);
+    $s_result = $query_result->fetch_all(MYSQLI_ASSOC);
+    print_r(json_encode($s_result));
+?>
