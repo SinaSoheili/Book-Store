e723ecccffce70dbaeb02ab9d4f7c9d96743a549
SINA <sinasoheili79@gmail.com>
2020-03-08 23:03:18 +0330

    create interface to handle like in server side

---
+++ Book_Store_Android/app/src/main/java/ir/sinasoheili/bookstore/PRESENTER/Like_API.java
@@ -0,0 +1,20 @@
+package ir.sinasoheili.bookstore.PRESENTER;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface Like_API
+{
+//    user_like_book.php ? type=[delete_like , insert_like , check_like] & user_id =  & book_id =
+    public static String base_url = "http://10.0.2.2/book_store_server/";
+
+    @GET("user_like_book.php?type=delete_like")
+    Call<String> delete_like(@Query("user_id") int user_id , @Query("book_id") int book_id);
+
+    @GET("user_like_book.php?type=insert_like")
+    Call<String> insert_like(@Query("user_id") int user_id , @Query("book_id") int book_id);
+
+    @GET("user_like_book.php?type=check_like")
+    Call<String> check_like(@Query("user_id") int user_id , @Query("book_id") int book_id);
+}
